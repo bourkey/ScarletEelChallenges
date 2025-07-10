be4568b73764658d9c8d1425ac31690d0a658240
codecowboydotio <scottvankalken@gmail.com>
2025-07-10 12:59:35 +1000

    shell script to install challenges

---
+++ add_challenges.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BASE_DIR="/root/ScarletEelChallenges/Offensive/"
+####
+#Install offensive questions
+####
+ctf install ctf challenge install ${BASE_DIR}/q1
+ctf install ctf challenge install ${BASE_DIR}/q2
